585fb817cdfd6f4ac0bad298fe6e389ba07764cf
mikinty <myou@andrew.cmu.edu>
2017-09-15 00:08:29 -0400

    added jquery to env

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,11 @@ module.exports = {
     es6: true,
     node: true,
     mocha: true,
+    jquery: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+  ],
   parser: 'babel-eslint',  
   parserOptions: {
     sourceType: 'module',
