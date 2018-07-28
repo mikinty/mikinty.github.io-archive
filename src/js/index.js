1a5ed987d724aec697a207a5f75cccb6d2fbdfbc
mikinty <myou@andrew.cmu.edu>
2018-07-28 18:23:49 -0400

    implemented lazy loading for images. Video does not work

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,17 @@ import './scroll';
 // import uikit requirements
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+
+// import blazy for lazy loading images and videos
+import Blazy from 'blazy';
+
+let blazy = new Blazy({
+  // src: 'src',
+  loadInvisible: true,
+  success: (ele, msg) => {
+    console.log(ele, msg);
+  },
+  error: (ele, msg) => {
+    console.log(ele, msg);
+  }
+});
