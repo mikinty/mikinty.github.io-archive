50b3937d75ee2689629bb33560c8faa2fce1b1ba
mikinty <myou@andrew.cmu.edu>
2018-07-29 23:45:56 -0400

    be safe and only do this when the document is ready

--- src/js/index.js
+++ src/js/index.js
@@ -10,15 +10,17 @@ import Icons from 'uikit/dist/js/uikit-icons';
 // import blazy for lazy loading images and videos
 import Blazy from 'blazy';
 
-let blazy = new Blazy({
-  // we need this to load the images that are not shown in the slideshow
-  loadInvisible: true,
-  /* Debugging purposes only
-  success: (ele) => {
-    console.log('Success', ele);
-  },
-  error: (ele, msg) => {
-    console.log(ele, msg);
-  }
-  */
+$(document).ready(function() {
+  let blazy = new Blazy({
+    // we need this to load the images that are not shown in the slideshow
+    loadInvisible: true,
+    /* Debugging purposes only
+    success: (ele) => {
+      console.log('Success', ele);
+    },
+    error: (ele, msg) => {
+      console.log(ele, msg);
+    }
+    */
+  });
 });
